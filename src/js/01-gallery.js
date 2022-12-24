4cd7ca29b3a913de76ae2d9d802fe2af78b5030a
serheihimself <sergm080@gmail.com>
2022-12-24 20:16:15 +0200

    task 1 commit

---
+++ src/js/01-gallery.js
@@ -0,0 +1,21 @@
+import { galleryItems } from './gallery-items';
+// Change code below this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+
+// ! Получил доступ к диву
+const galleryEl = document.querySelector('.gallery');
+
+// ! Создал разметку
+const markup = galleryItems.reduce
+((acc,{preview, original, description}) => acc +
+`<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt='${description}'/>
+</a>`
+,'');
+
+// ! Добавил разметку
+galleryEl.insertAdjacentHTML('beforeend', markup);
+
+new SimpleLightbox('.gallery a');
