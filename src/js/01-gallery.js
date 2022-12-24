07bce8c5ce25f51d32531380168908872cfda26c
serheihimself <sergm080@gmail.com>
2022-12-24 19:26:29 +0200

    fix commit

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,31 +1,24 @@
+
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
-
-console.log(galleryItems);
-
 import SimpleLightbox from 'simplelightbox';
 
-import 'simplelightbox/dist/simple-lightbox.min.css';
+console.log(galleryItems);
+const imagesContainer = document.querySelector('.gallery');
+const imagesMarkup = galleryItems
+  .map(
+    ({ preview, description, original }) =>
+      `<a class="gallery__item" href="${original}">
+		<img class="gallery__image" src="${preview}" alt="${description}" />
+	</a>`
+  )
+  .join('');
 
-const gallery = document.querySelector('.gallery');
-const items = [];
-
-galleryItems.forEach(element => {
-  const galleryLink = document.createElement('a');
-  galleryLink.className = 'gallery__link';
-  galleryLink.href = element.original;
-  const galleryImage = document.createElement('img');
-  galleryImage.className = 'gallery__image';
-  galleryImage.src = element.preview;
-  galleryImage.setAttribute('title', element.description);
-  galleryImage.alt = element.description;
-
-  galleryLink.append(galleryImage);
-  items.push(galleryLink);
-});
-gallery.append(...items);
+imagesContainer.insertAdjacentHTML('beforeend', imagesMarkup);
 
 new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
   captionDelay: 250,
+  captionPosition: 'bottom',
 });
